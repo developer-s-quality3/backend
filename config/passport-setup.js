3fbe5b0916a2a6995782bf21a225892fcd6e2b73
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-11-01 23:55:37 -0700

    config changes

---
+++ config/passport-setup.js
@@ -0,0 +1,16 @@
+const passport = require('passport');
+const KakaoStrategy = require('passport-kakao').Strategy;
+
+passport.use(
+  new KakaoStrategy(
+    {
+      clientID: process.env.KAKAO_RESTAPI_KEY, // REST API key
+      callbackURL: '/oauth/kakao/callback',
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(profile);
+    }
+  )
+);
